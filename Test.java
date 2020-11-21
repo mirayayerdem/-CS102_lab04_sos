162bf3662c7816dcefb8f403261d0681cf7a1ea3
miray <mirayayerdem@gmail.com>
2020-11-21 14:10:43 +0300

    Test Class has created

---
+++ Test.java
@@ -0,0 +1,9 @@
+
+import cs101.sosgame.SOS;
+
+public class Test {
+    public static void main(String[] args) {
+        SOS a = new SOS(3);
+        System.out.println(a.getDimension());
+    }
+}
