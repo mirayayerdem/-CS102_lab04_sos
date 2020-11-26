a578292cc884911eb5e5a78465606a9ea153ad3a
miray <mirayayerdem@gmail.com>
2020-11-26 10:50:30 +0300

    SOSGUIPanel Class has been modified

--- SOSGUIPanel.java
+++ SOSGUIPanel.java
@@ -1,82 +1,95 @@
+/**
+ * a class creates the panel and its components
+ * author: Miray Ayerdem
+ * version : 25/11/2020
+ */
 import cs101.sosgame.SOS;
-
 import javax.swing.*;
-import javax.xml.ws.Action;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+
 public class SOSGUIPanel extends JPanel {
+    //Instances
     private SOSCanvas canvas;
     private SOS game;
     private int space;
-    private String p1Name;
-    private String p2Name;
+    private String p1Name, p2Name;
     private final int WIDTH = 500;
     private final int HEIGHT = 600;
     private final int COMPONENTS_HEIGHT = 100;
-    private final ImageIcon logo = new ImageIcon(getClass().getResource("wall13.png"));
     private boolean oSelected, sSelected;
     private JPanel components;
-    private JLabel p1, p2;
+    private JLabel p1, p2 , label;
     private JRadioButton oButton, sButton;
     private ActionListener buttonListener;
-    private MyMouseListener mListener;
     private ButtonGroup buttonGroup;
 
-    SOSGUIPanel(SOS obj, String p1, String p2)
+    //Constructor
+    SOSGUIPanel(SOS obj, String p1, String p2) //game obj, p1 name, p2 name as parameters
     {
        this.game = obj;
        this.p1Name = p1;
        this.p2Name = p2;
        canvas = new SOSCanvas(game);
        space = canvas.getSpace();
+       label = new JLabel("SOS Game : " + p1 + " VS. " + p2);
        setPreferredSize(new Dimension(WIDTH, HEIGHT));
-       buttonListener = new MyRatioButtonListener();
+       buttonListener = new MyRadioButtonListener();
        buttonGroup = new ButtonGroup();
        createComponentsPanel();
        canvas.addMouseListener(new MyMouseListener());
        this.setLayout(new BorderLayout());
-       this.add(new JLabel(logo), BorderLayout.NORTH);
+       this.add(label, BorderLayout.NORTH);
        this.add(canvas, BorderLayout.CENTER);
        this.add(components, BorderLayout.SOUTH);
     }
+
+    /**
+     * a method the create components to this panel
+     */
     public void createComponentsPanel()
     {
         components = new JPanel();
+        JPanel labelPanel = new JPanel();
         components.setPreferredSize(new Dimension(WIDTH, COMPONENTS_HEIGHT));
-        components.setLayout(new FlowLayout());
+        components.setLayout(new BorderLayout());
         p1 = new JLabel(p1Name + ":" + game.getPlayerScore1());
         p2 = new JLabel(p2Name + ":" + game.getPlayerScore2());
         p1.setOpaque(true);
         p2.setOpaque(true);
         p1.setBackground (Color.GREEN);
-        sButton = new JRadioButton();
-        sButton.addActionListener(buttonListener);
-        buttonGroup.add(sButton);
-        oButton = new JRadioButton();
-        oButton.addActionListener(buttonListener);
-        buttonGroup.add(oButton);
+        sButton = new JRadioButton("S");
+        sButton.addActionListener(buttonListener); //adding listener
+        buttonGroup.add(sButton); //adding button group to be able to choose only one of the buttons
+        oButton = new JRadioButton("O");
+        oButton.addActionListener(buttonListener); //adding listener
+        buttonGroup.add(oButton);  //adding button group to be able to choose only one of the buttons
         sButton.setSelected(true);
-        components.add(p1);
-        components.add(sButton);
-        components.add(oButton);
-        components.add(p2);
+        labelPanel.add(p1);
+        labelPanel.add(sButton);
+        labelPanel.add(oButton);
+        labelPanel.add(p2);
+        components.add(labelPanel, BorderLayout.CENTER);
+        components.setBackground(Color.lightGray);
 
 
     }
-    class MyRatioButtonListener implements ActionListener
+    //inner class to listen the button actions
+    class MyRadioButtonListener implements ActionListener
     {
+        @Override
         public void actionPerformed(ActionEvent e)
         {
-            if(e.getSource() == sButton)
+            if(e.getSource() == sButton) //if sButton is clicked, set the button as true
             {
                 sButton.setSelected(true);
                 oButton.setSelected(false);
             }
-            else
+            else //if oButton is clicked, set the button as true
             {
                 oButton.setSelected(true);
                 sButton.setSelected(false);
@@ -84,7 +97,6 @@ public class SOSGUIPanel extends JPanel {
 
         }
     }
-
     class MyMouseListener extends MouseAdapter
     {
         SOSGUIPanel panel;
@@ -93,17 +105,18 @@ public class SOSGUIPanel extends JPanel {
         public void mousePressed(MouseEvent e)
         {
             String champion;
-            if(e.getX() >= space && e.getX() <= canvas.getEndOfX() && e.getY() >= space && e.getY() <= canvas.getEndOfY())
+            if(e.getX() >= space && e.getX() <= canvas.getEndOfX() && e.getY() >= space && e.getY() <= canvas.getEndOfY()) //checks if the canvas contains the point
             {
-                if(sButton.isSelected())
+
+                if(sButton.isSelected()) //play the s letter in the pressed row and column
                 {
-                    game.play('s', (int)(((e.getY() - space) / space)) + 1, (int)(((e.getX() - space) / space)) + 1);
+                    game.play('s', (int)(((e.getY() - canvas.getStartOfY()) / space)) + 1, (int)(((e.getX() - canvas.getStartOfX()) / space)) + 1);
                 }
-                else
+                else // play the o letter in the pressed row and column
                 {
-                    game.play('o',  (int)(((e.getY() - space) / space)) + 1, (int)(((e.getX() - space) / space)) + 1);
+                    game.play('o',  (int)(((e.getY() - canvas.getStartOfY()) / space)) + 1, (int)(((e.getX() - canvas.getStartOfX()) / space)) + 1);
                 }
-                if(game.getTurn() == 1)
+                if(game.getTurn() == 1) // if the turn of first player his/her name is showed colorful
                 {
                     p1.setBackground(Color.GREEN);
                     p2.setBackground(Color.WHITE);
@@ -113,23 +126,23 @@ public class SOSGUIPanel extends JPanel {
                     p2.setBackground(Color.GREEN);
                     p1.setBackground(Color.WHITE);
                 }
-                p1.setText(p1Name + ":" + game.getPlayerScore1());
-                p2.setText(p2Name + ":" + game.getPlayerScore2());
-                canvas.repaint();
-                game.printBoard();
+                p1.setText(p1Name + ":" + game.getPlayerScore1()); //shows the score
+                p2.setText(p2Name + ":" + game.getPlayerScore2()); //shows the score
+                canvas.repaint(); //call the paintComponent method
+
                 if(game.getPlayerScore1() > game.getPlayerScore2())
                 {
-                    champion = p1Name;
+                    champion = p1Name; //states the leader
                 }
                 else if(game.getPlayerScore2() > game.getPlayerScore1())
                 {
-                    champion = p2Name;
+                    champion = p2Name; //states the leader
                 }
                 else
                 {
-                    champion = "draw";
+                    champion = "draw"; //there is no winner
                 }
-                if(game.isGameOver()  && !champion.equals("draw") )
+                if(game.isGameOver()  && !champion.equals("draw") ) //if the game is over, winner is declared with messageDialog
                 {
                     JOptionPane.showMessageDialog(new JFrame() , "Winner is " + champion);
                 }
